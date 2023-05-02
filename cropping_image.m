4ba667287bd0abf55dff87485a6cb06fed768aac
Tousif <tousifulhaque@iut-dhaka.edu>
2023-05-01 20:27:24 -0500

    Fix: Code clean mat

--- cropping_image.m
+++ cropping_image.m
@@ -1,35 +1,35 @@
+function cropping_image(crop_ratio, non_face_path, face_path, mat_file_name)
 
-function non_face_training = cropping_image()
-% non face croping
-% non face croping
-
-%%
-face_training = dir('data/training_faces/*.bmp');
-non_face_training_list = dir('data/training_nonfaces/*.jpg');
-crop_ratio = 1;
-
+% getting the path of all test files
 s = filesep;
-% size_list = size(face_training,1);
-file_path = [face_training(1).folder ,s, face_training(1).name];
+non_face_pattern = [non_face_path, s, '*.jpg'];
+face_pattern = [face_path , s, '*.bmp'];
+face_list = dir(face_pattern);
+non_face_list = dir(non_face_pattern);
+
+%reading one train image to get size
+file_path = [face_list(1).folder ,s, face_list(1).name];
 img = read_gray(file_path);
 train_face_size = size(img,1);
 
+%taking a random window and cropping it
 number_of_random_points = 50;
-non_face_training = zeros(round(train_face_size*crop_ratio),round(train_face_size*crop_ratio),size(non_face_training_list,1) * number_of_random_points);
+nonface_images = zeros(round(train_face_size*crop_ratio),round(train_face_size*crop_ratio),size(non_face_list,1) * number_of_random_points);
 non_face_image_index = 1;
-for i = 1 : size(non_face_training_list,1)
-    file_path = [non_face_training_list(i).folder ,s, non_face_training_list(i).name];
+for i = 1 : size(non_face_list,1)
+    file_path = [non_face_list(i).folder ,s, non_face_list(i).name];
     img = read_gray(file_path);
     row_range = size(img,1) - train_face_size;
     col_range = size(img,2) - train_face_size;
-%     disp([row_range col_range]);
+
     row_points = randi([1 row_range], number_of_random_points);
     col_points = randi([1 col_range], number_of_random_points);
     for j = 1 : number_of_random_points
-        non_face_training(:,:,non_face_image_index) = img(row_points(j):row_points(j)+99,col_points(j):col_points(j)+99);
+        nonface_images(:,:,non_face_image_index) = img(row_points(j):row_points(j)+99,col_points(j):col_points(j)+99);
         non_face_image_index = non_face_image_index + 1;
     end
 end
-save('training_nonface.mat', "non_face_training");
-%%
+
+save(mat_file_name, 'nonface_images');
+
 end
--- make_face_mat.m
+++ make_face_mat.m
@@ -1,10 +1,9 @@
-function face_images = make_face_mat(path, mat_file_name)
-crop_ratio  = 1;
+function make_face_mat(path, mat_file_name, crop_ratio)
 s = filesep;
 pattern = [path , s , '*.bmp'];
-face_training = dir(pattern);
-test_face = imread([face_training(1).folder ,s, face_training(1).name]);
-total_face = size(face_training,1);
+face_list = dir(pattern);
+test_face = imread([face_list(1).folder ,s, face_list(1).name]);
+total_face = size(face_list,1);
 img_shape = size(test_face);
 face_images = zeros(round(img_shape(1)*crop_ratio), round(img_shape(2)*crop_ratio), total_face);
 upper = ceil((img_shape(1) * crop_ratio) /2);
@@ -13,7 +12,7 @@ row_center = img_shape(1) / 2;
 col_center = img_shape(2) / 2;
 
 for i = 1:total_face
-     img = imread([face_training(i).folder ,s, face_training(i).name]);
+     img = imread([face_list(i).folder ,s, face_list(i).name]);
      face_images(:,:,i) = img(row_center-upper+1 : row_center+lower, col_center - upper+1 : col_center + lower); 
 end
 
